8684957f9d2caa823feae9b80d97e45cee2f24e9
Robert Krepets <r.b.krepets@gmail.com>
2018-07-28 19:54:45 +0500

    настройки для одного bundle.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 //import $ from 'jquery';
-import { Greeter } from './greeter.js';
+//import { Greeter } from './greeter.js';
 
-import './styles.scss';
+import styles from './styles.scss';
+import logo from './car.png';
 
-const greeter = new Greeter();
+// const greeter = new Greeter();
 
-const message = greeter.greet('Hello', 'Webpack');
+// const message = greeter.greet('Hello', 'Webpack');
+//
+// const app = document.querySelector('#app');
 
-const app = document.querySelector('#app');
+const img = document.createElement('img');
+img.className = styles.logo;
+img.src = logo;
 
 const h1 = document.createElement('h1');
 h1.textContent = 'Знакомство с Webpack';
+h1.className = styles.title;
 
 app.appendChild(h1);
+app.appendChild(img);
 
 //$('#app').text(message);
-console.log(message, 233);
+console.log(styles);
+console.log(logo);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const extractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -7,8 +8,6 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
     },
-
-    mode: 'development',
 
     watch: true,
 
@@ -23,8 +22,26 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: extractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 3000,
+                        name: '[name].[ext]',
+                        outputPath: 'img/'
+                    }
+                }
             }
         ]
-    }
+    },
+
+    plugins: [
+        new extractTextPlugin('styles.css')
+    ]
 };
